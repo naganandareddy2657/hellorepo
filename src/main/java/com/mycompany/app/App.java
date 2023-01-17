a3f150cd04c0748408481140df2cc8577f10e4ca
naganandareddy2657 <110278902+naganandareddy2657@users.noreply.github.com>
2023-01-17 02:53:03 +0000

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        int num=20;
+        int num=-1;
         if(num>0)
             System.out.println("Positive Number");
         else if(num==0)
